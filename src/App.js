62aacb50d48818fc5907ce7569b53a1bcb88599d
Serhat Ozturk <so2turk@gmail.com>
2022-01-11 15:00:01 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import HomePage from './pages/home';
 import About from './pages/about';
 import ArticlePage from './pages/article';
 import ArticleListPage from './pages/article-list-page';
+import NavBar from './navbar';
 import Err404 from './pages/err404';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <div id='page-body'>
           <Routes>
             <Route path="/" element={<HomePage/>} exact />
