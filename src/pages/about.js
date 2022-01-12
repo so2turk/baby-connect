c4a502bb8b9fd98293c75b42169dbafe5443be30
Serhat Ozturk <so2turk@gmail.com>
2022-01-12 11:24:32 +0100

    Add dummy content to home, about and article-content pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,15 @@ import React from "react";
 const AboutPage = () => (
   <>
     <h1>About</h1>
-    <p>Lorem</p>
-    <p>Ipsum</p>
+    <p>
+      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent ornare at urna vel aliquam. Quisque ac sodales augue, sit amet egestas odio. Duis elit ante, tempus vitae urna vel, lobortis pretium neque. Suspendisse risus libero, volutpat vitae commodo nec, pellentesque in eros. Nulla dignissim orci in placerat finibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean nec augue in mi pretium hendrerit consectetur non purus. Maecenas blandit quam a ligula blandit molestie. Etiam malesuada aliquet eleifend.
+    </p>
+    <p>
+      Morbi sed molestie orci. Ut pretium turpis imperdiet luctus aliquet. Donec est lacus, placerat quis pretium non, dictum eget dui. Pellentesque finibus vel ante ut tincidunt. Suspendisse sit amet tellus sollicitudin, sodales velit a, semper turpis. Donec ut feugiat orci. Quisque rutrum risus ut mauris porta tincidunt. Vestibulum gravida nulla leo. Aenean sed vestibulum odio. Maecenas porta posuere sem imperdiet tincidunt. Praesent eu neque in nulla aliquet commodo a vel dui. Sed volutpat feugiat urna sit amet cursus. Sed dictum ultrices justo ac ultricies.
+    </p>
+    <p>
+      Mauris iaculis ipsum felis, non vestibulum velit convallis finibus. Ut euismod id urna a tempor. In dictum volutpat purus, ut varius arcu tempor sit amet. Mauris sit amet tincidunt massa, quis lacinia ligula. Pellentesque pharetra ligula consequat, tincidunt urna vitae, faucibus eros. Ut tincidunt luctus finibus. Curabitur rhoncus, augue eget ornare facilisis, nisi velit pretium dolor, at rhoncus nunc tellus at leo. Sed vel feugiat felis.
+    </p>
   </>
 )
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,8 +3,12 @@ import React from "react";
 const HomePage = () => (
   <>
     <h1>Welcome to my blog</h1>
-    <p>Lorem</p>
-    <p>Ipsum</p>
+    <p>
+      Sed pulvinar mi sit amet feugiat dignissim. Proin lacus quam, luctus in eros vitae, gravida dignissim nisi. Aenean eget est vitae urna pretium porttitor. Nulla non commodo metus. Fusce imperdiet faucibus eleifend. Sed mattis dictum mauris. Pellentesque rutrum velit ac sapien gravida, ut consectetur sem eleifend. Phasellus suscipit laoreet ipsum, non placerat arcu efficitur ut. Vestibulum eleifend quam et facilisis aliquet. Duis quis lectus tortor. Vestibulum et sapien et lacus sollicitudin lobortis quis in sem. Phasellus cursus nibh metus, sed porttitor libero malesuada in. Vivamus suscipit mauris in velit auctor sagittis.
+    </p>
+    <p>
+      In hac habitasse platea dictumst. Mauris et dui libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam at dapibus odio. In hac habitasse platea dictumst. Cras mauris odio, fermentum vel risus non, mollis mollis enim. Nunc quis risus et mi pellentesque tristique. Sed pulvinar mattis quam, nec fermentum lacus placerat nec. Etiam convallis dictum enim ac maximus. Donec porttitor ligula libero, eu eleifend ante cursus ut. Suspendisse sit amet vehicula lorem. Pellentesque vitae metus blandit nisl feugiat mattis.
+    </p>
   </>
 )
 
