e4cf8e179e7f4691caf2cc523fa1cf8722e20b01
Serhat Ozturk <so2turk@gmail.com>
2022-01-12 11:32:17 +0100

    Create a component for dynamic article lists

--- src/pages/article-list-page.js
+++ src/pages/article-list-page.js
@@ -1,19 +1,9 @@
 import React from "react";
 import articleContent from './article-content'
-import { Link } from "react-router-dom";
 
 const ArticleListPage = () => (
   <>
     <h1>Articles</h1>
-    { articleContent.map((a, key) => {
-      return (
-        <Link className='article-list-item' key={ key } to={ `/article/${ a.name }` }>
-          <h3>{ a.title }</h3>
-          
-          <p>{ a.content[0].substring(0,100)}..</p>
-        </Link>
-    )})}
-    
   </>
 )
 
