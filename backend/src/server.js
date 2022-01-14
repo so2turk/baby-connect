caaf2f2fd13bc8f6adad98a970a2c3313f70eef9
Serhat Ozturk <so2turk@yahoo.com>
2022-01-14 12:49:49 +0100

    Set up basic post/get requests

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,13 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// Setting up the requests
 app.get('/hello', (req, res) => res.send('Hi there'))
+app.get('/hello/:name', (req, res) => res.send(`Hi ${req.params.name}`))
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}. You ok?`))
+
 
 app.listen(8000, () => console.log('Listening on port 8000'))
