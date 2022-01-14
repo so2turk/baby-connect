c2c73d9b8f1418ad36604f90df9e71f5c84d1850
Serhat Ozturk <so2turk@yahoo.com>
2022-01-14 12:53:15 +0100

    Add upvoting get request for the articles

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,28 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-// Setting up the requests
-app.get('/hello', (req, res) => res.send('Hi there'))
-app.get('/hello/:name', (req, res) => res.send(`Hi ${req.params.name}`))
-app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}. You ok?`))
+const articleInfo = {
+  'learn-react': {
+    upvotes : 0
+  },
+  'learn-node': {
+    upvotes : 0
+  },
+  'become-a-jr': {
+    upvotes : 0
+  }
+}
 
+// Setting up the requests
+// app.get('/hello', (req, res) => res.send('Hi there'))
+// app.get('/hello/:name', (req, res) => res.send(`Hi ${req.params.name}`))
+// app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}. You ok?`))
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name
+  articleInfo[articleName].upvotes += 1
+
+  res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes.`)
+})
 
 app.listen(8000, () => console.log('Listening on port 8000'))
