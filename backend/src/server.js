10d6b888ea4dd93ccac4f93376284bc9e6c4f26a
Serhat Ozturk <so2turk@yahoo.com>
2022-01-17 17:15:08 +0100

    Add commenting post request for the articles

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,13 +7,16 @@ app.use(express.urlencoded({extended:true}))
 
 const articleInfo = {
   'learn-react': {
-    upvotes : 0
+    upvotes : 0,
+    comments: []
   },
   'learn-node': {
-    upvotes : 0
+    upvotes : 0,
+    comments: []
   },
   'become-a-jr': {
-    upvotes : 0
+    upvotes : 0,
+    comments: []
   }
 }
 
@@ -22,11 +25,18 @@ const articleInfo = {
 // app.get('/hello/:name', (req, res) => res.send(`Hi ${req.params.name}`))
 // app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}. You ok?`))
 
+// Post request for upvoting the articles
 app.post('/api/articles/:name/upvote', (req, res) => {
   const articleName = req.params.name
   articleInfo[articleName].upvotes += 1
-
   res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes.`)
+})
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const { username, text } = req.body
+  const articleName = req.params.name
+  articleInfo[articleName].comments.push({ username, text })
+  res.status(200).send(articleInfo[articleName])
 })
 
 app.listen(8000, () => console.log('Listening on port 8000'))
