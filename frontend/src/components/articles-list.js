c8fe4bb89defc420e08de0ab444325abcdd08183
Serhat Ozturk <so2turk@yahoo.com>
2022-01-24 14:38:01 +0100

    Clean up and set website title

--- frontend/src/components/articles-list.js
+++ frontend/src/components/articles-list.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const ArticlesList = ( { articles }) => (
   <>
-    { articles.map((a, key) => (
-        <Link className='article-list-item' key={ key } to={ `/article/${ a.name }` }>
+    { articles.map((a, idx) => (
+        <Link className='article-list-item' key={ idx } to={ `/article/${ a.name }` }>
           <h3>{ a.title }</h3>
           <p>{ a.content[0].substring(0,150) }..</p>
         </Link>
