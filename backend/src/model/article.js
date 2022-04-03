69727e4ec9aac3cbdcae7b1a0898ae74ac35c477
Serhat Ozturk <so2turk@yahoo.com>
2022-04-03 18:48:52 +0200

    Refactor article model

--- backend/src/model/article.js
+++ backend/src/model/article.js
@@ -1,16 +1,50 @@
 import mongoose from 'mongoose'
+import Comment from './comment'
+import User from './user'
 
 const articleSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  upvotes: Number,
-  comments: []
+	author: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: User,
+			autopopulate: { maxDepth: 1 },
+		},
+	],
+	title: {
+		type: String,
+		required: true,
+	},
+	content: [
+		{
+			type: String,
+			required: true,
+		},
+	],
+	link: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	images: [
+		{
+			type: String,
+		},
+	],
+	likes: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: User,
+		},
+	],
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: Comment,
+		},
+	],
 })
 
-class Article { }
+class Article {}
 
 articleSchema.loadClass(Article)
 export default mongoose.model('Article', articleSchema)
