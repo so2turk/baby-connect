38edca9b3c9b4b39c2463d077ea99512d94b3d96
Serhat Ozturk <so2turk@yahoo.com>
2022-04-23 17:20:07 +0200

    Create other routes requiring authorization

--- backend/src/router/users-router.js
+++ backend/src/router/users-router.js
@@ -1,8 +1,23 @@
 import express from 'express'
+import {
+	createUser,
+	deleteUser,
+	getUser,
+	login,
+	refreshUserToken,
+	updateUser,
+	userDetails,
 } from '../controller/users-controller.js'
 import { verify } from '../util/auth.js'
 const usersRouter = express.Router()
 
-usersRouter.post('/', createUser).get('/:userId', getUser)
+usersRouter
+	.post('/register', createUser)
+	.post('/login', login)
+	.get('/details', verify, userDetails)
+	.get('/details/:userId', verify, getUser)
+	.delete('/delete/:userId', verify, deleteUser)
+	.patch('/update/:userId', verify, updateUser)
+	.post('/refresh', refreshUserToken)
 
 export default usersRouter
