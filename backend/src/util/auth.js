3c7d9b5265d4b3bc1879e9d6d26030d27d4b9db6
Serhat Ozturk <so2turk@yahoo.com>
2022-05-12 21:35:34 +0200

    Update genAccessToken and verify functions

--- backend/src/util/auth.js
+++ backend/src/util/auth.js
@@ -2,24 +2,33 @@ import jwt from 'jsonwebtoken'
 import User from '../model/user.js'
 const accessTokenKey = process.env.ACCESS_TOKEN_KEY
 
-export const genAccessToken = (user) => {
-	return jwt.sign({ id: user._id, isAdmin: user.isAdmin }, accessTokenKey, {
-		expiresIn: '20m',
-	})
+export const genAccessToken = (date, user) => {
+	return jwt.sign(
+		{
+			date: date,
+			userName: user.userName,
+			id: user._id,
+			isAdmin: user.isAdmin,
+		},
+		accessTokenKey,
+		{
+			expiresIn: '10s',
+		}
+	)
 }
 
 export const verify = (req, res, next) => {
-	const authHeader = req.headers.authorization
+	const authHeader = req.headers.authorization || req.headers.Authorization
 
-	if (!authHeader) return res.status(401).json('Authorization failed: no token')
+	if (!authHeader) return res.status(401).send('Authorization failed: no token')
 	else if (!authHeader.startsWith('Bearer'))
-		return res.status(401).json('Authorization failed: token is not valid')
+		return res.status(401).send('Authorization failed: token is not valid')
 
 	const token = authHeader.split(' ')[1]
 
 	jwt.verify(token, accessTokenKey, async (err, user) => {
 		if (err)
-			return res.status(403).json('Authorization failed: token is not valid')
+			return res.status(403).send('Authorization failed: token is not valid')
 
 		req.user = await User.findById(user.id).select('-password')
 		next()
